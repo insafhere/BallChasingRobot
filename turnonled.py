9dc92512993d6e6855c3bceec339b4911f6bddc1
insafhere <84378807+insafhere@users.noreply.github.com>
2021-10-26 16:26:04 +0800

    Update turnonled.py

--- turnonled.py
+++ turnonled.py
@@ -9,7 +9,9 @@ GPIO.setup(LED_PIN,GPIO.OUT)
 
 while True:
     GPIO.output(LED_PIN,GPIO.HIGH)
+    print("Led On")
     time.sleep(0.1)
     GPIO.output(LED_PIN,GPIO.LOW)
     time.sleep(0.1)
+    print("Led Off")
 
