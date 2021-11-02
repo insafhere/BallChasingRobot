d6470794253a8e5c3461502240fe59025fd83bcf
Muhamed Insaf Zainudin <84378807+insafhere@users.noreply.github.com>
2021-11-02 09:56:08 +0800

    Update roboballchase.py

--- roboballchase.py
+++ roboballchase.py
@@ -125,7 +125,7 @@ for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
       #grab the raw NumPy array representing the image, then initialize the timestamp and occupied/unoccupied text
       frame = image.array
       frame=cv2.flip(frame,1)
-      cv2.imshow("frame", image)
+      cv2.imshow("frame", frame)
       global centre_x
       global centre_y
       centre_x=0.
