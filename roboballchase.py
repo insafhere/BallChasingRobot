6d661ec711533ce7a5c1516c6f3708a642afe9cc
Muhamed Insaf Zainudin <84378807+insafhere@users.noreply.github.com>
2021-11-02 09:59:05 +0800

    Update roboballchase.py

--- roboballchase.py
+++ roboballchase.py
@@ -72,18 +72,21 @@ def stop():
      
 #Image analysis work
 def segment_colour(frame):    #returns only the red colors in the frame
-    hsv_roi =  cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask_1 = cv2.inRange(hsv_roi, np.array([45,47,169]), np.array([90,255,255]))
-    ycr_roi=cv2.cvtColor(frame,cv2.COLOR_BGR2YCrCb)
-    mask_2=cv2.inRange(ycr_roi, np.array((0.,165.,0.)), np.array((255.,255.,255.)))
+      hsv_roi =  cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+      mask_1 = cv2.inRange(hsv_roi, np.array([45,47,169]), np.array([90,255,255]))
+      ycr_roi=cv2.cvtColor(frame,cv2.COLOR_BGR2YCrCb)
+      mask_2=cv2.inRange(ycr_roi, np.array((0.,165.,0.)), np.array((255.,255.,255.)))
 
-    mask = mask_1 | mask_2
-    kern_dilate = np.ones((8,8),np.uint8)
-    kern_erode  = np.ones((3,3),np.uint8)
-    mask= cv2.erode(mask,kern_erode)      #Eroding
-    mask=cv2.dilate(mask,kern_dilate)     #Dilating
-    #cv2.imshow('mask',mask)
-    return mask
+      mask = mask_1 | mask_2
+      kern_dilate = np.ones((8,8),np.uint8)
+      kern_erode  = np.ones((3,3),np.uint8)
+      mask= cv2.erode(mask,kern_erode)      #Eroding
+      mask=cv2.dilate(mask,kern_dilate)     #Dilating
+
+      cv2.imshow('mask',mask)
+      cv2.imshow('mask1',mask_1)
+      cv2.imshow('mask2',mask_2)
+      return mask
 
 def find_blob(blob): #returns the red colored circle
     largest_contour=0
